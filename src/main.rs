4109b5350f4b3dc27f1eae71165678c79657544c
ather <atherfawaz@gmail.com>
2021-04-08 13:19:15 +0500

    Added file reading for source code.

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ mod file_reader;
 
 fn main() {
     let source_code = String::from(file_reader::read_file("src/source_code.c"));
-    println!("Source code: \n{}", source_code);
+    println!("SOURCE CODE: \n{}", source_code);
+
+    let mut split = source_code.split(" ");
+
+    let vec: Vec<&str> = split.collect();
+
+    println!("Tokens: \n{}", 1);
+
 }

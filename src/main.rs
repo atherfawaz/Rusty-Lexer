fa339894adc97b3284652b047653c27452b0a295
ather <atherfawaz@gmail.com>
2021-04-09 10:35:12 +0500

    Started parsing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 fn build_tokens(token_vector: Vec<&str>) -> HashMap<&str, &str> {
     let mut token_map = HashMap::new();
     for elem in &token_vector {
+        println!("Parsing: {}", elem);
         let one_tok: Vec<&str> = elem.split(",").collect();
         token_map.insert(one_tok[0], one_tok[1]);
     }
@@ -16,5 +17,16 @@ fn main() {
     let tokens_str = String::from(file_reader::read_tokens("src/TOKENS.txt"));
     let token_map: HashMap<&str, &str> = build_tokens(tokens_str.split(" ").collect());
     println!("\nRESERVED WORDS: \n{:?}", token_map);
-    println!("\nCODE SPLIT: \n{:?}", split_code);
+    println!("\nCODE SPLIT: \n{:?}\n", split_code);
+
+    let mut tokens = Vec::new();
+    println!("BUILDING TOKENS:");
+    for &elem in &split_code {
+        match token_map.get(elem) {
+            Some(map_found) => tokens.push((elem, map_found)),
+            None => println!("Calling manual_parser() for {}", elem),
+        }
+    }
+
+    println!("\nGENERATED TOKENS: \n{:?}", tokens);
 }
